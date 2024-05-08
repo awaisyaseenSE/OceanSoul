16ad57d29338709ae59cbe99d26eb51e7b19eb1a
awaisyaseenSE <148967337+awaisyaseenSE@users.noreply.github.com>
2024-05-08 16:55:19 +0500

    before setup firebase final

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,16 +1,10 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-import LottieView from 'lottie-react-native';
 
 export default function HomeScreen() {
   return (
     <View style={{marginTop: 20}}>
-      <LottieView
-        source={require('../assets/animation/loader.json')}
-        style={{width: 80, height: 80}}
-        loop
-        autoPlay
-      />
+      <Text>HomeScreen</Text>
     </View>
   );
 }
