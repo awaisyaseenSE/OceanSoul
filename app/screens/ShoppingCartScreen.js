80478d764a0f8892a81ebbeb118fe6b332d54f92
awaisyaseenSE <148967337+awaisyaseenSE@users.noreply.github.com>
2024-05-17 17:24:40 +0500

    friday work us

--- app/screens/ShoppingCartScreen.js
+++ app/screens/ShoppingCartScreen.js
@@ -1,16 +1,19 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
 import ScreenComponent from '../components/ScreenComponent';
 import colors from '../styles/colors';
+import FastImage from 'react-native-fast-image';
 
 export default function ShoppingCartScreen() {
   return (
     <>
       <ScreenComponent style={{backgroundColor: colors.white_light2}}>
-        <View>
+        <View style={{flex: 1, paddingHorizontal: 20}}>
           <Text>Shopping Cart Screen</Text>
         </View>
       </ScreenComponent>
     </>
   );
 }
+
+const styles = StyleSheet.create({});
