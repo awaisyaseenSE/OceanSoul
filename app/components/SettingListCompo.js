6eb54d2858f56a710db19fc696397fba5c1e9e37
awaisyaseenSE <148967337+awaisyaseenSE@users.noreply.github.com>
2024-05-17 09:32:02 +0500

    add icons for setting screen

---
+++ app/components/SettingListCompo.js
@@ -0,0 +1,62 @@
+import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import React from 'react';
+import fontFamily from '../styles/fontFamily';
+import colors from '../styles/colors';
+
+const SettingListCompo = ({title = '', onPress, icon}) => {
+  return (
+    <TouchableOpacity style={styles.container} activeOpacity={0.6}>
+      <View style={styles.row}>
+        <View style={styles.iconContainer}>
+          <Image source={icon} style={styles.icon} />
+        </View>
+        <Text style={styles.heading}>{title}</Text>
+      </View>
+
+      <Image source={require('../assets/next.png')} style={styles.nextIcon} />
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+
+    marginBottom: 10,
+    paddingVertical: 4,
+  },
+  heading: {
+    fontFamily: fontFamily.semi_bold,
+    color: colors.black,
+    fontSize: 14,
+    marginLeft: 12,
+  },
+  nextIcon: {
+    width: 14,
+    height: 14,
+    resizeMode: 'contain',
+    tintColor: colors.gray_dark,
+  },
+  icon: {
+    width: 20,
+    height: 20,
+    resizeMode: 'contain',
+    tintColor: colors.white,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  iconContainer: {
+    width: 36,
+    height: 36,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: colors.red,
+    borderRadius: 4,
+  },
+});
+
+export default SettingListCompo;
--- app/screens/SettingScreen.js
+++ app/screens/SettingScreen.js
@@ -1,17 +1,39 @@
-import {View, Text, ActivityIndicator} from 'react-native';
-import React, {useEffect, useState} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
 import ScreenComponent from '../components/ScreenComponent';
 import colors from '../styles/colors';
-import constants from '../constants/constants';
+import fontFamily from '../styles/fontFamily';
+import SettingListCompo from '../components/SettingListCompo';
 
 export default function SettingScreen() {
   return (
     <>
       <ScreenComponent style={{backgroundColor: colors.white_light2}}>
-        <View>
-          <Text>Setting Screen</Text>
+        <View style={styles.container}>
+          <Text style={styles.heading}>Settings</Text>
+          <SettingListCompo
+            icon={require('../assets/apple.png')}
+            title="Edit Profile"
+          />
+          <SettingListCompo
+            icon={require('../assets/apple.png')}
+            title="Edit Profile"
+          />
         </View>
       </ScreenComponent>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+  },
+  heading: {
+    fontSize: 20,
+    color: colors.black,
+    fontFamily: fontFamily.bold,
+    marginBottom: 50,
+  },
+});
