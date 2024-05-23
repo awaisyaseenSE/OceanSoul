21affa33be8e2789f96f97b7cac361f641c01f1d
awaisyaseenSE <148967337+awaisyaseenSE@users.noreply.github.com>
2024-05-23 16:18:52 +0500

    add file

--- app/screens/WishListScreen.js
+++ app/screens/WishListScreen.js
@@ -2,8 +2,10 @@ import {View, Text} from 'react-native';
 import React from 'react';
 import ScreenComponent from '../components/ScreenComponent';
 import colors from '../styles/colors';
+// import {REACT_APP_RAPID_API_KEY} from '@env';
 
 export default function WishListScreen() {
+  console.log(process.env.RAPID_API_KEY);
   return (
     <>
       <ScreenComponent style={{backgroundColor: colors.white_light2}}>
